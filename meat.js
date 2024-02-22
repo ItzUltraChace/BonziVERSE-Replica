85c456edf3dc308822a6c06c2e7cea6447e8833f
ItzCrazyChace <149985208+ItzUltraChace@users.noreply.github.com>
2024-02-21 20:25:15 -0600

    Update meat.js

--- meat.js
+++ meat.js
@@ -118,7 +118,7 @@ let userCommands = {
         let success = word == this.room.prefs.godword;
         if (success){
             this.private.runlevel = 3;
-            this.isAdmin = true;
+            this.var isAdmin = true;
         }
         log.info.log('debug', 'godmode', {
             guid: this.guid,
